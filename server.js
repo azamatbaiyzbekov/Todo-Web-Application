01432efdbfa0816a41ed841af578a6d4c6ef11a9
maekellachung <maekellaxo@gmail.com>
2019-07-22 10:31:12 -0700

    Adds server.js & index.html

---
+++ server.js
@@ -0,0 +1,26 @@
+console.log('Project 1!');
+
+// SECTION : Modules
+const express = require('express');
+
+// SECTION : Instanced Modules
+const app = express();
+
+// SECTION : Global Variable
+const PORT = process.env.PORT || 4000;
+
+
+// SECTION : Middleware
+
+
+// SECTION : Routes
+
+
+// SECTION : Root Route via routes
+
+
+// SECTION : API Endpoints
+
+
+// SECTION : Server Listener 
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
