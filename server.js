8b761d5ab0c12ae4057729348d9d2e59bb960ead
maekellachung <maekellaxo@gmail.com>
2019-07-22 10:54:44 -0700

    Adds public/views directory

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ const PORT = process.env.PORT || 4000;
 
 
 // SECTION : Middleware
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // SECTION : Routes
+
+
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/status', (req,res) => {
+  res.json({ status: 200, message: 'Okkkkk I see you'});
+});
+
+// Root Route
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
 
 
 // SECTION : Root Route via routes
