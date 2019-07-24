589cb6270806d473e2ec7867a801f13a7c4c4218
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-07-23 20:50:22 -0700

    adds add and delete functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -167,3 +167,81 @@ dateElement.innerText = today.toLocaleDateString('en-US', options)
 // newListForm.addEventListener('submit', addNewList);
 
 // myList.addEventListener('click', handleListSectionClick);
+
+
+const addbutton = document.getElementById('add');
+const inputTask = document.getElementById('new-task');
+const unfinishedTasks = document.getElementById('unfinished-tasks');
+const finishedTasks = document.getElementById('finished-tasks');
+
+function createNewElement (task) {
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('button');
+  checkbox.className="material-icons checkbox";
+  checkbox.innerHTML=`<i class="material-icons">check_box_outline_blank</i>`;
+  const label = document.createElement('label');
+  label.innerText=task;
+  const input = document.createElement('input');
+  input.type="text";
+  const editButton = document.createElement('button');
+  editButton.className="material-icons edit";
+  editButton.innerHTML=`<i class="material-icons">edit</i>`;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.className="material-icons delete";
+  deleteButton.innerHTML=`<i class="material-icons">delete</i>`;
+
+  listItem.appendChild(checkbox)
+  listItem.appendChild(label)
+  listItem.appendChild(input)
+  listItem.appendChild(deleteButton)
+  listItem.appendChild(editButton)
+
+  return listItem;
+ 
+
+
+}
+
+function addTask() {
+  if(inputTask.value) {
+    const listItem = createNewElement(inputTask.value);
+    unfinishedTasks.appendChild(listItem)
+    bindTaskEvents(listItem, finishTask)
+    inputTask.value="";
+  }
+}
+
+addbutton.addEventListener('click', addTask);
+
+
+function deleteTask() {
+  const listItem = this.parentNode;
+  console.log(listItem);
+  const ul = listItem.parentNode;
+  ul.removeChild(listItem)
+
+}
+
+function editTask() {
+
+}
+
+function finishTask() {
+
+}
+
+function unfinishTask() {
+
+}
+
+function bindTaskEvents(listItem, checkboxEvent ) {
+  const checkbox = listItem.querySelector('button.checkbox');
+  const editButton = listItem.querySelector('button.edit');
+  const deleteButton = listItem.querySelector('button.delete');
+
+  checkbox.onclick=checkboxEvent;
+  editButton.onclick=editTask;
+  deleteButton.addEventListener('click', deleteTask)
+
+}
