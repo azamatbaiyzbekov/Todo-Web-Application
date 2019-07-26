669144da509f4d77f6751ef50bdd14f30407dfb0
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-07-25 21:42:53 -0700

    adds Add/Create/Delete json endpoints

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -14,34 +14,13 @@ const BASE_URL = '/api/users/current';
 const listSection = document.getElementById('list');
 
 const toDoTemplate = (list) => {
-	return `<div id="${list._id}">
-	<h4>${list.typeOfList}</h4>
+	return `<div class='list-card' id="${list._id}">
+  <h4>${list.typeOfList}</h4>
+  <input name='task' id="task"><button>Add Task</button>
 	<button class="delete-button">&times;</button> 
 	<button class="edit-button">edit</button>
 	</div>`
   }
-
-
-//   const editToDo = (event) => {
-    
-//     const ToDoName = event.target.parentNode.children[0].innerText;
-//     const ToDoDescription = event.target.parentNode.children[1].innerText;
-//     event.target.parentNode.inneHTML = `
-//     <h4>Edit ${ToDoName}</h4>
-//       <form>
-//         <div>
-//           <label for="cityName">City Name</label>
-//           <input type="text" id="editCityName" name="name" value="${ToDoName}"/>
-//         </div>
-//         <div>
-//           <label for="cityDescription">City Descriptioin</label>
-//           <input type="text" id="editCityDescription" name="description" value="${ToDoDescription}"/>
-//         </div>
-//         <button type="button" class="cancel-edit">Cancel</button>
-//         <button class="submit-edit">Submit</button>
-//       </form>
-//     `;
-//   };
 
   
 
@@ -57,9 +36,11 @@ const render = (state) => {
 
   
 const LIST_URL = '/lists';
+// const TASK_URL = '/tasks';
 
 const text = document.getElementById('ToDo');
 const addList = document.getElementById('add-list');
+const addTask = document.getElementById('task')
 
 
 let state = {
@@ -79,25 +60,6 @@ let state = {
   }
   
 getUser();
-
-// // SECTION TEMPLATE OUTPUT
-// const listTemplate = (list) => {
-//   return `
-//     <div class="return-info" >
-// ​
-//     <div class="dog-cards" id="${list._id}">
-//       <h4>Name:</h4>
-//       <p class="form-text">${task.data}</p>
-//       <h4>Age:</h4>
-//       <p class="form-text">${task.dateadded}</p>
-      
-//       <button class="btn btn-danger delete-button">Delete</button>
-//       <button class="btn btn-success edit-button">Edit</button>
-      
-//     </div>
-//     </div>
-//   `;
-// }
 
 
  const addNewList = (event) => {
@@ -125,7 +87,7 @@ getUser();
      };
 console.log(addList)
 
-
+ 
 const deleteList = (event) => {
   const listId = event.target.parentNode.id;
   fetch(`${LIST_URL}/${listId}`, {
@@ -135,22 +97,86 @@ const deleteList = (event) => {
     .then((data) => getUser())
     .catch((err) => console.log(err));
 }
-​
-
-// const handleListSectionClick = (event) => {
-//   event.preventDefault();
-//   if (event.target.classList.contains('edit-button')) {
-//     editList(event);
-//   } else if (event.target.classList.contains('cancel-edit')) {
-//     getUser();
-//   } else if (event.target.classList.contains('delete-button')) {
-//     deleteList(event);
-//   }
-// }
 
 
+const editToDo = (event) => {
+  const toDoName = event.target.parentNode.children[0].innerText;
+  console.log(toDoName)
+  event.target.parentNode.innerHTML = `
+  <h4>Edit ${toDoName}</h4>
+    <form>
+      <div>
+        <label for="listName">ToDo Name</label>
+        <input type="text" id="editListName" name="name" value="${toDoName}"/>
+      </div>
+      <button type="button" class="cancel-edit">Cancel</button>
+      <button class="submit-edit">Submit</button>
+    </form>
+  `;
+};
+
+const updateList = (event) => {
+  listId = event.target.parentNode.parentNode.id;
+    const listName = document.getElementById('editListName').value;
+    const newList= { name: listName, };
+  fetch(`${LIST_URL}/${listId}`, {
+    method: 'PUT',
+    headers: { 'Content-type': 'application/json'},
+    body: JSON.stringify(newList)
+  })
+  .then((res) => res.json())
+  .then((data) => getUser())
+  .catch((err) => console.log(err));
+};
+
+
+const includeTask = (event) => {
+  event.preventDefault();
+  const task = document.getElementById('task')
+ 
+  const newTask = { task: task.value };
+
+  console.log(newTask)
+      
+  fetch(LIST_URL, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json', },
+    body: JSON.stringify(newTask),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+    state.lists = json.data;
+    console.log(json)
+    // state.lists = json.data.lists;
+    // render(state);
+    // window.location.reload();
+    task.value = '';
+    task.focus();
+    })
+    .catch((err) => console.log(err))
+    };
 
 
 
-     addList.addEventListener('click', addNewList);
-    //  button.addEventListener('click', addNewList);
+
+const handleListSectionClick = (event) => {
+  event.preventDefault();
+   if(event.target.classList.contains('delete-button')) {
+    deleteList(event);
+  } else if(event.target.classList.contains('edit-button')) {
+    editToDo(event);
+  } else if (event.target.classList.contains('submit-edit')) {
+    updateList(event)
+}
+
+}
+
+addList.addEventListener('click', addNewList);
+
+listSection.addEventListener('click', handleListSectionClick);
+
+addTask.addEventListener('click', includeTask)
+
+
+
+
